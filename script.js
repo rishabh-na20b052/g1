f1c7158536eff0e2dcebbbf57547b7680de35acd
Rishabh Upadhyay <87112835+rishabh-na20b052@users.noreply.github.com>
2022-06-30 03:49:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let n; n = prompt("Enter Player Name"); 
+[lbl] beginning:
 document.getElementById("pr").innerHTML= "Hello"+" "+ n +"!!";
 let x=document.getElementById("boy"); let tt = document.getElementById("t");
 document.getElementById("0").style.background=0;
@@ -144,7 +145,7 @@ document.getElementById("84").style.background=(a[8][4]?"rgb(2, 255, 255)":"rgb(
 }
 function bas() {
   setTimeout(() => {
-    document.getElementById("start").setAttribute("onclick","location.reload()");
+    document.getElementById("start").setAttribute("onclick","()=>{goto beginning;}");
     document.getElementById("start").disabled=0;
     tt.style.display="none";
     x.style.height="45%";
@@ -152,7 +153,7 @@ function bas() {
     x.style.boxShadow = "0 0 10vw red";
   }, 600);
   setTimeout(() => {
-    if(confirm(n+" and Jojo scored "+(s-1)+". Click OK to retry.")==true)location.reload();
+    if(confirm(n+" and Jojo scored "+(s-1)+". Click OK to retry.")==true){()=>{goto beginning;}};
   }, 1800);
 }
 function tm0() {
